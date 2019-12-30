ec24a4270fa358ec61d2837f02671cc4cc7dc76d
dkqqkr42 <dkqqkr42@gmail.com>
2019-12-30 15:25:04 +0900

    변수추가 및 입출력 추가

--- hello.c
+++ hello.c
@@ -1,6 +1,9 @@
 #include<stdio.h>
 #include<math.h>
 void main(){
-printf("Hello World!\n");
-printf("Happy");
+	int a;
+	printf("Hello World!\n");
+	printf("Happy\n");
+	scanf("%d",&a);
+	printf("%d",a);
 }
